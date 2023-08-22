4a7130007683877b7ceb66fc4a4b5efcc7dcb4e0
miguello <miguello@titamedia.com>
2023-08-22 18:13:14 -0500

    ci(Workflow): 🎨 se ajustan path para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom'
-  }
-})
+	base: '/myteam-website/',
+	plugins: [react()],
+	test: {
+		environment: 'jsdom',
+	},
+});
