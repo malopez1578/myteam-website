9a79a526a872d9e441aeae2e6336c017ddcd2407
malopez1578 <malopez1578@gmail.com>
2023-11-11 14:02:11 -0500

    fix: refactor App.test.tsx imports and remove unnecessary tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 // Imports
-import { fireEvent, render, screen } from '@testing-library/react';
-import { beforeAll, describe, expect, it } from 'vitest';
+import { render } from '@testing-library/react';
+import { beforeAll, describe } from 'vitest';
 
 // To Test
 import App from './App';
@@ -9,44 +9,5 @@ describe('Renders main page correctly', async () => {
 	beforeAll(async () => {
 		// Setup
 		render(<App />);
-	});
-	/**
-	 * Passes - shows title correctly
-	 */
-	it('Should render the page correctly', async () => {
-		const h1 = screen.queryByText('Vite + React');
-
-		// Post Expectations
-		expect(h1).not.toBeNull();
-	});
-
-	/**
-	 * Passes - shows the button count correctly present
-	 */
-	it('Should show the button count set to 0', async () => {
-		// Setup
-		const button = screen.queryByText('count is 0');
-
-		// Expectations
-		expect(button).not.toBeNull();
-	});
-
-	/**
-	 * Passes - clicks the button 3 times and shows the correct count
-	 */
-	it('Should show the button count set to 3', async () => {
-		// Setup
-		const button = screen.queryByTestId('count');
-
-		// Pre Expectations
-		expect(button).not.toBeNull();
-
-		// Actions
-		fireEvent.click(button as HTMLElement);
-		fireEvent.click(button as HTMLElement);
-		fireEvent.click(button as HTMLElement);
-
-		// Post Expectations
-		expect(button?.innerHTML).toBe('count is 3');
 	});
 });
